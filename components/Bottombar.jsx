9bdccefd7315bfba5993a90b771b1040776075f6
Aum-Barai <aumbarai2004@gmail.com>
2025-03-07 16:07:11 +0530

    updated link in componet

--- components/Bottombar.jsx
+++ components/Bottombar.jsx
@@ -11,7 +11,7 @@ const Bottombar = () => {
     <footer className={styles.bottomBar}>
       <div className={styles.container}>
         <a
-          href="https://github.com/AumBarai/vscode-portfolio"
+          href="https://github.com/Aum-Barai/vscode-portfolio"
           target="_blank"
           rel="noreferrer noopener"
           className={styles.section}
